3431e3dfebba542f8f23f3593f821b756fdcc468
rahul kumar kasaudhon <68266050+rahulg311@users.noreply.github.com>
2022-03-10 11:08:11 +0530

    Add files via upload

---
+++ scroolBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export const scroolBar = () => {
+  return (
+    <div>
+{/* scrool baar html */}
+
+
+
+
+
+{/* overflow-auto   h-180" id="style-2" */}
+
+
+
+
+
+{/* scrool css */}
+{/* #style-2::-webkit-scrollbar {
+		width: 12px;
+		background-color: #F5F5F5;
+	  }
+	  
+	  #style-2::-webkit-scrollbar-thumb {
+		border-radius: 10px;
+	   border: 10px solid rgba(50, 46, 46, 0.05);
+		transform: rotate(-90deg);
+	  } */}
+
+
+    </div>
+  )
+}
